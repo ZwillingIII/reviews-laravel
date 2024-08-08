b172d34977d08f6c07d4b104be076ba26cfc21e4
zwilling <zwill@bokus.ru>
2024-08-08 09:31:53 +0300

    Доработан тестовый pipeline

--- app/Helpers/PostHelper.php
+++ app/Helpers/PostHelper.php
@@ -4,10 +4,14 @@ namespace App\Helpers;
 
 class PostHelper
 {
+    // TODO: Тестовый pipeline
     public function handle($query, $next)
     {
+        $query->merge([
+            'title' => 'А вот и нет',
+            'text' => 'Нормальный текст запили'
+        ]);
 
-
-        return $query;
+        return $next($query);
     }
 }
